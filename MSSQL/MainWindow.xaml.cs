4e6399ad2766bb02283d7f4789b94931035ad2b7
chimingkuei <chimingkuei@gmail.com>
2025-08-09 09:24:04 +0800

    MSSQL

---
+++ MSSQL/MainWindow.xaml.cs
@@ -0,0 +1,231 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MSSQL
+{
+    #region Config Class
+    public class SerialNumber
+    {
+        [JsonProperty("Parameter1_val")]
+        public string Parameter1_val { get; set; }
+        [JsonProperty("Parameter2_val")]
+        public string Parameter2_val { get; set; }
+    }
+
+    public class Model
+    {
+        [JsonProperty("SerialNumbers")]
+        public SerialNumber SerialNumbers { get; set; }
+    }
+
+    public class RootObject
+    {
+        [JsonProperty("Models")]
+        public List<Model> Models { get; set; }
+    }
+    #endregion
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region Function
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("請問是否要關閉？", "確認", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        #region Config
+        private SerialNumber SerialNumberClass()
+        {
+            SerialNumber serialnumber_ = new SerialNumber
+            {
+                Parameter1_val = Parameter1.Text,
+                Parameter2_val = Parameter2.Text
+            };
+            return serialnumber_;
+        }
+
+        private void LoadConfig(int model, int serialnumber, bool encryption = false)
+        {
+            List<RootObject> Parameter_info = Config.Load(encryption);
+            if (Parameter_info != null)
+            {
+                Parameter1.Text = Parameter_info[model].Models[serialnumber].SerialNumbers.Parameter1_val;
+                Parameter2.Text = Parameter_info[model].Models[serialnumber].SerialNumbers.Parameter2_val;
+            }
+            else
+            {
+                // 結構:2個Models、Models下在各2個SerialNumbers
+                SerialNumber serialnumber_ = SerialNumberClass();
+                List<Model> models = new List<Model>
+                {
+                    new Model { SerialNumbers = serialnumber_ },
+                    new Model { SerialNumbers = serialnumber_ }
+                };
+                List<RootObject> rootObjects = new List<RootObject>
+                {
+                    new RootObject { Models = models },
+                    new RootObject { Models = models }
+                };
+                Config.SaveInit(rootObjects, encryption);
+            }
+        }
+
+        private void SaveConfig(int model, int serialnumber, bool encryption = false)
+        {
+            Config.Save(model, serialnumber, SerialNumberClass(), encryption);
+        }
+        #endregion
+
+        #region Dispatcher Invoke 
+        public string DispatcherGetValue(System.Windows.Controls.TextBox control)
+        {
+            string content = "";
+            this.Dispatcher.Invoke(() =>
+            {
+                content = control.Text;
+            });
+            return content;
+        }
+
+        public void DispatcherSetValue(string content, System.Windows.Controls.TextBox control)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                control.Text = content;
+            });
+        }
+
+        #region IntegerUpDown Invoke
+        //public int? DispatcherIntegerUpDownGetValue(Xceed.Wpf.Toolkit.IntegerUpDown control)
+        //{
+        //    int? content = null;
+        //    this.Dispatcher.Invoke(() =>
+        //    {
+        //        if (int.TryParse(control.Text, out int result))
+        //        {
+        //            content = result;
+        //        }
+        //        else
+        //        {
+        //            content = null;
+        //        }
+        //    });
+        //    return content;
+        //}
+        #endregion
+        #endregion
+
+        private void OpenFolder(string description, System.Windows.Controls.TextBox textbox)
+        {
+            System.Windows.Forms.FolderBrowserDialog path = new System.Windows.Forms.FolderBrowserDialog();
+            path.Description = description;
+            path.ShowDialog();
+            textbox.Text = path.SelectedPath;
+        }
+
+        private void WriteVersionToFile()
+        {
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory;  // 執行檔目錄
+            string assemblyInfoPath = System.IO.Path.GetFullPath(System.IO.Path.Combine(baseDir, @"..\..\..\Properties\AssemblyInfo.cs"));
+            if (File.Exists(assemblyInfoPath))
+            {
+                // 讀取檔案內容
+                string content = File.ReadAllText(assemblyInfoPath);
+                // 使用正則表示式擷取 AssemblyFileVersion
+                Regex regex = new Regex(@"\[assembly:\s*AssemblyFileVersion\s*\(\s*""(?<version>[\d\.]+)""\s*\)\s*\]");
+                Match match = regex.Match(content);
+                if (match.Success)
+                {
+                    string version = match.Groups["version"].Value;
+                    // 將版本寫入 TXT 檔案
+                    string outputPath = "AssemblyVersion.txt";
+                    File.WriteAllText(outputPath, version);
+                }
+            }
+        }
+        #endregion
+
+        #region Parameter and Init
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadConfig(0, 0);
+        }
+        BaseConfig<RootObject> Config = new BaseConfig<RootObject>();
+        BaseLogRecord Logger = new BaseLogRecord();
+        #endregion
+
+        #region Main Screen
+        private void Main_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            switch ((sender as System.Windows.Controls.Button).Name)
+            {
+                case nameof(Demo):
+                    {
+                        Console.OutputEncoding = System.Text.Encoding.UTF8;
+                        string connectionString = "Server=localhost\\MSSQL2022;Database=Quotation;Trusted_Connection=True;";
+                        string query = "SELECT * FROM LightSource";
+                        using (SqlConnection conn = new SqlConnection(connectionString))
+                        {
+                            try
+                            {
+                                conn.Open();
+                                Console.WriteLine("✅ 連線成功！");
+
+                                using (SqlCommand cmd = new SqlCommand(query, conn))
+                                using (SqlDataReader reader = cmd.ExecuteReader())
+                                {
+                                    while (reader.Read())
+                                    {
+                                        Console.WriteLine(
+                                        $"ID: {reader["LightSourceID"],-5} | " +
+                                        $"類別: {reader["Category"],-10} | " +
+                                        $"型號: {reader["Model"],-10} | " +
+                                        $"標準價: {Convert.ToDecimal(reader["StandardPrice"]):N2}, " +
+                                        $"折扣價: {(reader["DiscountPrice"] == DBNull.Value ? "無折扣" : Convert.ToDecimal(reader["DiscountPrice"]).ToString("N2"))}"
+                                        );
+                                    }
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("❌ 錯誤：" + ex.Message);
+                            }
+                        }
+                        break;
+                    }
+            }
+        }
+        #endregion
+
+
+    }
+}
